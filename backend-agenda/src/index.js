4e6970c909635e280829f48ddcf3c3f62c0fdb11
pedrinchip1221 <pedrogames910@gmail.com>
2023-11-03 14:44:24 -0500

    Instalacion de cors y su configuracion

--- backend-agenda/src/index.js
+++ backend-agenda/src/index.js
@@ -1,5 +1,6 @@
 const express  = require('express')
 const {port: APP_PORT, port} = require('./config/config')
+const cors = require('cors')
 const routerApi = require('./routes')
 const router = require('./routes/users.routes')
 
@@ -7,9 +8,10 @@ const router = require('./routes/users.routes')
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 routerApi(app)
 
-app.listen(port, () => {
-    console.log(`app running on ${port}`)
+app.listen(APP_PORT, () => {
+    console.log(`app running on ${APP_PORT}`)
 })
